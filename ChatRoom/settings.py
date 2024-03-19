5dd3e127834f392bcc96d50d3c41c35613d1d9f7
Vimal-Raj-S <vimalraj5875@gmail.com>
2024-03-19 12:29:39 +0530

    chat-room

--- ChatRoom/settings.py
+++ ChatRoom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+=zsb@j#mkn05-ng+(rgrn%-sqozi^*w^lo_w!^fk-bpmiio9#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
